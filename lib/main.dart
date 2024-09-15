8730c763d8c54a262b9825969aa5bd269933382a
Kamil Akbarov <komil79@yandex.ru>
2024-09-15 19:29:55 +0300

    ДЗ 3 v1

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'widgets/foodListCards.dart';
+import 'widgets/foodlist_cards.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,90 +14,82 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Otus Food',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.grey),
-        useMaterial3: true,
-        fontFamily: 'Roboto'
-      ),
-      home: const FoodList(),
+      theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.grey), useMaterial3: true, fontFamily: 'Roboto'),
+      home: FoodList(),
     );
   }
 }
 
-class FoodList extends StatelessWidget {  
-  const FoodList({super.key});
+class FoodList extends StatelessWidget {
+  FoodList({super.key});
+
+  final List foodListData = [
+    <String, String>{
+      'cardText': 'Лосось в соусе терияки',
+      'cardUrl': '',
+      'cardImage': 'assets/images/f1.png',
+      'time': '45 минут',
+    },
+    <String, String>{
+      'cardText': 'Поке боул с сыром тофу',
+      'cardUrl': '',
+      'cardImage': 'assets/images/f2.png',
+      'time': '30 минут',
+    },
+    <String, String>{
+      'cardText': 'Стейк из говядины по-грузински с кукурузой',
+      'cardUrl': '',
+      'cardImage': 'assets/images/f3.png',
+      'time': '1 час 45 минут',
+    },
+    <String, String>{
+      'cardText': 'Тосты с голубикой и бананом',
+      'cardUrl': '',
+      'cardImage': 'assets/images/f4.png',
+      'time': '45 мину',
+    },
+    <String, String>{
+      'cardText': 'Паста с морепродуктами',
+      'cardUrl': '',
+      'cardImage': 'assets/images/f5.png',
+      'time': '25 минут',
+    },
+    <String, String>{
+      'cardText': 'Бургер с двумя котлетами',
+      'cardUrl': '',
+      'cardImage': 'assets/images/f6.png',
+      'time': '1 час',
+    },
+    <String, String>{
+      'cardText': 'Пицца Маргарита домашняя',
+      'cardUrl': '',
+      'cardImage': 'assets/images/f7.png',
+      'time': '25 минут',
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: const Color.fromRGBO(236, 236, 236, 1),
         title: const Text('Otus Food'),
       ),
       body: Center(
-        child: ListView(
-          padding: const EdgeInsets.all(8),
-          children:const [
-            FoodCards(
-              cardText: 'Лосось в соусе терияки',
-              cardUrl: '',
-              horTitleGap: 16.0,
-              cardImage: 'assets/images/f1.png',
-              time: '45 минут',
-            ),
-            SizedBox(height: 16,),
-            FoodCards(
-              cardText: 'Поке боул с сыром тофу',
-              cardUrl: '',
-              horTitleGap: 16.0,
-              cardImage: 'assets/images/f2.png',
-              time: '30 минут',
-            ),
-            SizedBox(height: 16,),
-            FoodCards(
-              cardText: 'Стейк из говядины по-грузински с кукурузой',
-              cardUrl: '',
-              horTitleGap: 16.0,
-              cardImage: 'assets/images/f3.png',
-              time: '1 час 45 минут',
-            ),
-            SizedBox(height: 16,),
-            FoodCards(
-              cardText: 'Тосты с голубикой и бананом',
-              cardUrl: '',
-              horTitleGap: 16.0,
-              cardImage: 'assets/images/f4.png',
-              time: '45 минут',
-            ),
-            SizedBox(height: 16,),
-            FoodCards(
-              cardText: 'Паста с морепродуктами',
-              cardUrl: '',
-              horTitleGap: 16.0,
-              cardImage: 'assets/images/f5.png',
-              time: '25 минут',
-            ),
-            SizedBox(height: 16,),
-            FoodCards(
-              cardText: 'Бургер с двумя котлетами',
-              cardUrl: '',
-              horTitleGap: 16.0,
-              cardImage: 'assets/images/f6.png',
-              time: '1 час',
-            ),
-            SizedBox(height: 16,),
-            FoodCards(
-              cardText: 'Пицца Маргарита домашняя',
-              cardUrl: '',
-              horTitleGap: 16.0,
-              cardImage: 'assets/images/f7.png',
-              time: '25 минут',
-            ),
-            SizedBox(height: 32,),
-          ],
+          child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: ListView.separated(
+          separatorBuilder: (context, index) => const SizedBox(
+            height: 16,
+          ),
+          itemBuilder: (context, index) => FoodCards(
+            cardText: foodListData[index]['cardText'],
+            cardUrl: foodListData[index]['cardUrl'],
+            cardImage: foodListData[index]['cardImage'],
+            time: foodListData[index]['time'],
+          ),
+          itemCount: foodListData.length,
         ),
-      ),
-
+      )),
     );
   }
 }
---
+++ lib/widgets/foodlist_cards.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+class FoodCards extends StatelessWidget {
+  final String cardText;
+  final String cardUrl;
+  final String cardImage;
+  final String time;
+
+  const FoodCards({super.key, required this.cardText, required this.cardUrl, required this.cardImage, required this.time});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shadowColor: Colors.white,
+      elevation: 10,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(5),
+      ),
+      child: SizedBox(
+        width: double.infinity,
+        height: 136,
+        child: Container(
+          color: Colors.white,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Image(
+                image: AssetImage(cardImage),
+                width: 149,
+                height: 136,
+                fit: BoxFit.cover,
+              ),
+              const SizedBox(
+                width: 8,
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        cardText,
+                        style: const TextStyle(
+                          fontSize: 22.0,
+                          fontFamily: 'Roboto',
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                          height: 1,
+                        ),
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      Row(
+                        children: [
+                          const Image(
+                            image: AssetImage('assets/icons/clock_icon.png'),
+                            width: 16,
+                            height: 16,
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          Expanded(
+                            child: Text(
+                              time,
+                              style: const TextStyle(
+                                fontSize: 16.0,
+                                fontFamily: 'Roboto',
+                                fontWeight: FontWeight.w400,
+                                color: Colors.green,
+                                height: 1,
+                              ),
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
